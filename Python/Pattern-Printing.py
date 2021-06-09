614dd915e1412217840f3525fc5776433ec40d1c
Sakshi <thekrsakshi@gmail.com>
2021-06-09 19:38:15 +0530

    Pattern Printing

---
+++ Python/Pattern-Printing.py
@@ -0,0 +1,17 @@
+
+'''
+Question : Write a program to print the following pattern.
+
+        *
+        **
+        ***
+        ****
+        *****
+
+        Input : -
+        Output : Given pattern
+
+'''
+
+for i in range(1,6):
+    print("*" * i )
