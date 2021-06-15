18098905485fc16f4f293e814348ef9cf311215a
Sakshi <thekrsakshi@gmail.com>
2021-06-15 23:10:43 +0530

    Even or Odd

---
+++ Python/Even-Or-Odd.py
@@ -0,0 +1,11 @@
+# Question : Tocheck wether the number is even or odd 
+#       Input : User input a number
+#        Output : natural numbers
+
+N=int(input("Enter a number : "))
+print("\n THE ENTERED NUMBER IS  :\n",N)
+if (N%2==0):
+    print("\n The entered number is Even.\n")
+else:
+    print("\n The entered number is Odd.\n")
+
