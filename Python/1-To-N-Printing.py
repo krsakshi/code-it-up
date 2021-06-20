e7291c46828ecfbeb4642b0c94f7ce311f7996a7
Sakshi <thekrsakshi@gmail.com>
2021-06-20 23:22:25 +0530

    Print N numbers

---
+++ Python/1-To-N-Printing.py
@@ -0,0 +1,9 @@
+# Question : To print numbers from 1 to N
+#       Input : User input a number
+#       Output : Natural numbers
+
+n=int(input("enter the number : "))
+i=1
+while(i<=n):
+    print(i,end='\n')
+    i+=1
