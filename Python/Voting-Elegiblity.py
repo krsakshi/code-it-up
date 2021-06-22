be9c3b38253805021efb64da0000f76abc16b758
Sakshi <thekrsakshi@gmail.com>
2021-06-22 23:17:10 +0530

    Voting Elegiblity

---
+++ Python/Voting-Elegiblity.py
@@ -0,0 +1,10 @@
+#Question : To check wether a person is eledgible to vote or not
+#        Input : User input age
+#        Output : Eledgible or Not Eledgible 
+
+age= int(input("Enter the Age :"))
+if (age>=18):
+    print("Age ",age,"is elegible to vote.")
+else:
+    print("Age ",age,"is  not elegible to vote.")
+    
