a7d640b6ebfd3f6bafd69012dc32e13728ead292
Sakshi <85516345+krsakshi@users.noreply.github.com>
2022-01-25 14:29:36 +0530

    Added Sum of list elements.py

---
+++ Python/Sum of list elements.py
@@ -0,0 +1,15 @@
+#  Question : Program to find the sum of all numbers stored in a list
+#        Input : List of numbers
+#        Output : Sum of all the elements in the list
+
+# List of numbers
+numbers = [6, 5, 3, 8, 4, 2, 5, 4, 11]
+
+# variable to store the sum
+sum = 0
+
+# iterate over the list
+for val in numbers:
+    sum = sum+val
+
+print("The sum is", sum)
