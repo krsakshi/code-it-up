c63190833b06304ef26af8a14c4d8180f6963686
Sakshi <85516345+krsakshi@users.noreply.github.com>
2022-01-26 11:48:52 +0530

    ASCII value of a character

---
+++ Python/ASCII value of a character.py
@@ -0,0 +1,7 @@
+#  Question : Program to find the sum of all numbers stored in a list
+#        Input : A haracter
+#        Output : ASCII value of the character
+
+# Taking input
+a = input("Enter a character : ")
+print("The ASCII value of '" + a + "' is", ord(a))
