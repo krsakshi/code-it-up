1aea89b218970f6363398c98316bc17cdbce0552
Sakshi <85516345+krsakshi@users.noreply.github.com>
2024-06-17 23:46:24 +0530

    Create Bubble_Sort.py

---
+++ Python/Bubble_Sort.py
@@ -0,0 +1,17 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+      
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+
+def main():
+    arr = [64, 34, 25, 12, 22, 11, 90]
+    print("Original array:", arr)
+    bubble_sort(arr)
+    print("Sorted array:", arr)
